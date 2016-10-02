490b0fcffe05a98747f1e591700805350cdf0ef2
alireza Akef <alireza.akef@gmail.com>
2016-10-02 19:30:11 +0330

    Initial Commit

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Alireza Akef on /11/71395AP.
+//  Copyright © 1395 AP Alireza Akefcom.dyna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
